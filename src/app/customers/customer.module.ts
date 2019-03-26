0e05ec5cf74e6a9faa3fe4e1abadc52401c637be
LuisDio <luis.diolindo@gmail.com>
2019-03-26 22:40:22 +0300

    initial project with two component

---
+++ src/app/customers/customer.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CustomersComponent } from './customers.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ CustomersComponent ],
+})
+export class AppModule { }
